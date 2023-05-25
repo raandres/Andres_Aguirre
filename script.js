4a21de51acd59541f70138f9e3c18fb4436ed424
Andres Aguirre <aandres51@hotmail.com>
2023-05-25 11:14:15 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,105 @@
+//*****************************************************************
+// Definicion de la estructura de operaciones
+
+function validarNumero(numero,mensaje)
+{
+	while(isNaN(numero))
+	{
+			alert("Ingreso un valor incorrecto, reintente")
+			numero = parseInt(prompt(mensaje));
+	}
+	return numero;
+}
+
+function mensajeBienvenida ()
+{
+	let mensajePresentacion = "Productos disponibles: \n Ingrese el numero del producto que desea \n";
+
+	PRODUCTOS.forEach(e => {
+		mensajePresentacion += `${e.id} - ${e.descripcion} - Stock: ${e.stock} - Precio: ${e.precio}\n`
+	})
+
+	let respuestaUser = parseInt(prompt(mensajePresentacion))
+	respuestaUser = validarNumero(respuestaUser,mensajePresentacion);
+
+	let productoSeleccionado = PRODUCTOS.find(elem => elem.id === respuestaUser);
+	
+	while(productoSeleccionado.stock === 0)
+	{
+		alert("El producto seleccionado no posee stock por el momento, por favor seleccione otro")
+		respuestaUser = parseInt(prompt(mensajePresentacion))
+		respuestaUser = validarNumero(respuestaUser,mensajePresentacion);
+		productoSeleccionado = PRODUCTOS.find(elem => elem.id === respuestaUser);
+	}
+	return productoSeleccionado;
+}
+
+function ingresarCantidad(producto)
+{
+	let precio = 0;
+	cantidadUser = parseInt(prompt(`Ingrese la cantidad de ${producto.descripcion} que desea`))
+	cantidadUser = validarNumero(cantidadUser, cantidadUser);
+	while(producto.stock < cantidadUser)
+	{
+		alert("Hay un problema, queres mas de lo que hay :)")
+		cantidadUser = parseInt(prompt(`Ingrese la cantidad de ${producto.descripcion} que desea`))
+		cantidadUser = validarNumero(cantidadUser, cantidadUser);
+	}
+	producto.stock -= cantidadUser;
+	producto.carrito += cantidadUser;
+	producto.cantidad += cantidadUser;
+	return producto.precio * cantidadUser;
+	
+}
+
+class Producto{
+    constructor (id, descripcion, stock, precio, carrito){
+        this.id = id,
+        this.descripcion = descripcion,
+		this.stock = stock,
+		this.precio = precio,
+		this.carrito = carrito,
+		this.cantidad = []
+    }
+}
+
+const producto1 = new Producto(1, "Galletas",0,1);
+const producto2 = new Producto(2, "Yerba",10,2);
+const producto3 = new Producto(3, "Azucar",10,3);
+const producto4 = new Producto(4, "Te",10,4);
+const producto5 = new Producto(5, "Arroz",10,5);
+
+const PRODUCTOS = [producto1,producto2,producto3,producto4,producto5];
+
+let continuar = true;
+let precioTotal = 0;
+let cantidadTotal = 0;
+let productosSeleccionados = [];
+let cantidadUser = 0;
+
+while (continuar)
+{
+
+	const productoSeleccionado = mensajeBienvenida();
+	const precioProducto = ingresarCantidad(productoSeleccionado);
+	
+	precioTotal += precioProducto;
+	cantidadTotal += cantidadUser;
+	productosSeleccionados.push(productoSeleccionado);
+		
+	const opcionContinuar = prompt("Desea continuar? Si/No").toLowerCase();
+	if(opcionContinuar !== "si" && opcionContinuar !== "sí")
+	{
+		continuar = false;
+	}
+		
+}
+
+let resumen1 = "Resumen de la seleccion: \n ------------------------------\nProductos seleccionados: \n";
+
+productosSeleccionados.forEach(producto => {
+		resumen1 += `- ${producto.descripcion}: $${producto.precio} x ${producto.cantidad} unidades\n`	});
+		
+resumen1 += `------------------------------\nCantidad total de productos: ${cantidadTotal}\nPrecio total de productos: $${precioTotal}`
+
+alert(resumen1);
